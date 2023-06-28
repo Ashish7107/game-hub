14bb10d37d64b526dd753111e56077d89295786f
Ashish7107 <yadavashish7107@gmail.com>
2023-06-28 16:53:12 +0530

    highlighted selected genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,11 +14,7 @@ const GameGrid = ({ selectedGenre }: Props) => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        padding={10}
-        spacing={3}
-      >
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} padding={10} spacing={3}>
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer key={skeleton}>
--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,23 +1,25 @@
 import { AxiosRequestConfig, CanceledError } from "axios";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 
 interface FetchResponse<T> {
   count: number;
   results: T[];
 }
-const useData = <T,>(
+
+const useData = <T>(
   endpoint: string,
   requestConfig?: AxiosRequestConfig,
   deps?: any[]
 ) => {
-  const [data, setdata] = useState<T[]>([]);
+  const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState("");
   const [isLoading, setLoading] = useState(false);
 
   useEffect(
     () => {
       const controller = new AbortController();
+
       setLoading(true);
       apiClient
         .get<FetchResponse<T>>(endpoint, {
@@ -25,7 +27,7 @@ const useData = <T,>(
           ...requestConfig,
         })
         .then((res) => {
-          setdata(res.data.results);
+          setData(res.data.results);
           setLoading(false);
         })
         .catch((err) => {
@@ -33,10 +35,13 @@ const useData = <T,>(
           setError(err.message);
           setLoading(false);
         });
+
       return () => controller.abort();
     },
     deps ? [...deps] : []
   );
+
   return { data, error, isLoading };
 };
+
 export default useData;
